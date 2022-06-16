b34576cbee0f13a3111059bb9959b1568913b94f
Jaakko Sirén <Jaakko.Siren@pm.me>
2022-06-17 02:40:49 +0300

    Add extraction scripts for other song books

---
+++ song_collections/README.md
@@ -0,0 +1,5 @@
+# TODO
+- Add m.tek.fi extraction
+- Add fiisut 4 extraction
+- Deltan laulukirja
+- https://rasputin.fi/
---
+++ song_collections/extract.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl 'https://emil.fi/p/lw3/kanta.json' -o emil_lauluwiki.json
+curl --insecure 'https://lauluwiki.fi/kanta.json' -o lauluwiki.json
